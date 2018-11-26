f05ea77ab52218f89c1ce3568f7f1dad5deb96bb
mickey_ichi <lemanhhungb01501@gmail.com>
2018-11-27 00:38:13 +0700

    refactor

--- datasources/db.datasource.js
+++ datasources/db.datasource.js
@@ -1,14 +1,8 @@
-const config = require('./db.datasource.json');
 const Datastore = require('nedb');
 
 class DbDataSource {
     constructor() {
-        this.config = config;
         this.db = new Datastore({ autoload: true });
-    }
-
-    getConfig() {
-        return this.config;
     }
 
     getDb() {
